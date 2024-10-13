7831e885ef30f2abb4b178760e12ceba0b274893
Xavikooo <149263342+Xavikooo@users.noreply.github.com>
2024-10-13 04:07:32 +0200

    v1

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function zaloguj() {
+   
+    const login = document.getElementById('login').value;
+    const haslo = document.getElementById('haslo').value;
+  
+    if (login === 'admin' && haslo === 'admin') {
+      window.location.href = 'next_page.html'; 
+    } else {
+      alert('Błędny login lub hasło');
+    }
+  }
+  
+
+function wyloguj() {
+  console.log("Wylogowano!");
+  window.location.href = 'index.html'; 
+  }
+
+function wysylkaformularza() {
+
+  const pesel = document.getElementById('pesel').value;
+  const Imie = document.getElementById('Imie').value;
+  const Nazwisko = document.getElementById('Nazwisko').value;
+
+  if (!pesel){
+    alert("Pole PESEL jest puste!")
+    return false;
+  }
+
+  if (!Imie){
+    alert("Pole Imie jest puste!")
+    return false;
+  }
+  if (!Nazwisko){
+    alert("Pole Nazwisko jest puste!")
+    return false;
+  }
+    console.log("Przesłano formularz");
+    window.location.href = 'koniec.html';
+    return false;
+
+  }
+
+
+  
